6adefe3e41b357c4ca283b7f920d2ae8776b2852
brandon-glem <31105149+brandon-glem@users.noreply.github.com>
2019-08-26 15:22:29 -0500

    cifrado aleatorio

--- clase 1.cpp
+++ clase 1.cpp
@@ -16,10 +16,19 @@ int main() {
 	string clave;
 	for (int i = 0; i < size(pal); i++) {
 		int num = rand() % (0 - 36);
+		cout << num;
 		clave += alf[num];
 	}
+	cout << endl;
 
-	cout << clave;
+	string cifrado;
+	for (int i = 0; i < size(pal); i++) {
+		int suma = alf.find(pal[i]) + alf.find(clave[i]);
+		cifrado+=alf[suma % 37];
+	}
+
+	cout << clave<<endl;
+	cout << cifrado;
 
 	
 
